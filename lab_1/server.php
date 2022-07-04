6463328d59d9b43e228ce6dff9568da2fcd8afe6
oleksandr_hrechenko <oleksandr_hrechenko@epam.com>
2022-07-04 17:18:20 +0200

    Added PDO interface to handle requests to DB layer

--- lab_1/server.php
+++ lab_1/server.php
@@ -4,61 +4,75 @@
     <input type="submit" value="Go to form"/>
 </form>
 <?php
-$host = '';
-$port = 5432;
-$dbname = '';
-$user = '';
-$password = '';
-$dbconn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+$dbconn = new PDO("", "", "");
 
 echo pathinfo();
 $n_l = "\n";
 
 
 if ($_POST["action"] == "new_nurse") {
-    $query = "SELECT count(*) FROM nurse";
-    $count_models = pg_fetch_row(pg_query($dbconn, $query))[0] or die("Cannot execute query: $query\n");
+
+    $statement = $dbconn->prepare("SELECT count(*) FROM nurse");
+    $statement->execute();
+
+
+    $count_models = pg_fetch_row(pg_query($dbconn, $statement))[0] or die("Cannot execute query: $statement\n");
 
     echo nl2br("Inserting new nurse... $n_l");
-    $query = "INSERT INTO nurse (id_nurse, name, date, department, shift) VALUES ($count_models + 1, '$_POST[name]','$_POST[date]', '$_POST[department]', '$_POST[shift]');";
-    $rs = pg_query($dbconn, $query) or die("Cannot execute query: $query\n");
+
+    $query = "INSERT INTO nurse (id_nurse, name, date, department, shift) VALUES (?, ?, ?, ?, ?);";
+    $stmt = $dbconn->prepare($query);
+    $stmt->execute([$count_models + 1, $_POST["name"], $_POST["date"], $_POST["department"], $_POST["shift"]]);
 }
 
 if ($_POST["action"] == "new_ward") {
-    $query = "SELECT count(*) FROM ward";
-    $count_models = pg_fetch_row(pg_query($dbconn, $query))[0] or die("Cannot execute query: $query\n");
+    $statement = $dbconn->prepare("SELECT count(*) FROM ward");
+    $statement->execute();
+    $count_models = $statement->fetch();
 
     echo nl2br("Inserting new ward... $n_l");
-    $query = "INSERT INTO ward (id_ward, name) VALUES ($count_models + 1, '$_POST[name]');";
-    $rs = pg_query($dbconn, $query) or die("Cannot execute query: $query\n");
+
+    $query = "INSERT INTO ward (id_ward, name) VALUES (?, ?);";
+    $stmt = $dbconn->prepare($query);
+    $stmt->execute([$count_models + 1, $_POST["name"]]);
 }
 
 if ($_POST["action"] == "new_nurse_ward") {
     echo nl2br("Inserting new ward... $n_l");
-    $query = "INSERT INTO nurse_ward (fid_nurse, fid_ward) VALUES ($_POST[id_nurse], $_POST[id_ward]);";
-    $rs = pg_query($dbconn, $query) or die("Cannot execute query: $query\n");
+
+    $query = "INSERT INTO nurse_ward (fid_nurse, fid_ward) VALUES (?, ?);";
+    $stmt = $dbconn->prepare($query);
+    $stmt->execute([$_POST["id_nurse"], $_POST["id_ward"]]);
+
 }
 
 
 $query = "SELECT * FROM nurse";
-$rs = pg_query($dbconn, $query) or die("Cannot execute query: $query\n");
-echo nl2br("Nurses $n_l");
-while ($row = pg_fetch_row($rs)) {
+$stmt = $dbconn->prepare($query);
+$stmt->execute();
+echo nl2br("$n_l Nurses $n_l");
+
+while ($row = $stmt->fetch()) {
     echo nl2br("$row[0] $row[1] $row[2] $row[3] $row[4] $n_l");
 }
 
-
 $query = "SELECT * FROM ward";
-$rs = pg_query($dbconn, $query) or die("Cannot execute query: $query\n");
+$stmt = $dbconn->prepare($query);
+$stmt->execute();
+
 echo nl2br("$n_l Wards $n_l");
-while ($row = pg_fetch_row($rs)) {
+
+while ($row = $stmt->fetch()) {
     echo nl2br("$row[0] $row[1] $row[2] $n_l");
 }
 
-$query = "SELECT * FROM nurse_ward";
-$rs = pg_query($dbconn, $query) or die("Cannot execute query: $query\n");
+
 echo nl2br("$n_l Nurse and wards $n_l");
-while ($row = pg_fetch_row($rs)) {
+$query = "SELECT * FROM nurse_ward";
+$stmt = $dbconn->prepare($query);
+$stmt->execute();
+
+while ($row = $stmt->fetch()) {
     echo nl2br("$row[0] $row[1] $row[2] $n_l");
 }
 
